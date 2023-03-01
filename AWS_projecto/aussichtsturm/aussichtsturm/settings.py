330dae56dde2907d84de29aa684d933d27183343
Pyrynets Serhii <srg6rt@gmail.com>
2023-03-01 23:30:52 +0200

    Add folders and files

--- AWS_projecto/aussichtsturm/aussichtsturm/settings.py
+++ AWS_projecto/aussichtsturm/aussichtsturm/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xn%st-x3q_wc1m$vn)4qgt*4#o!s46my009)#6zr36lah^zc_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +125,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xn%st-x3q_wc1m$vn)4qgt*4#o!s46my009)#6zr36lah^zc_&'
+
