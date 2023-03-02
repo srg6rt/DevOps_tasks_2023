403af38ef7f8af8e670a8cbb89bdb3aa4fb0b42e
Pyrynets Serhii <srg6rt@gmail.com>
2023-03-02 06:58:26 +0200

    Add folders and files

--- AWS_projecto/aussichtsturm/aussichtsturm/settings.py
+++ AWS_projecto/aussichtsturm/aussichtsturm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     "/aussichtsturm/youtube_comments_grabber/static",
 ]
--- main.tf
+++ main.tf
@@ -176,6 +176,7 @@ resource "aws_instance" "web-server-instance" {
 
                 "echo 'Install requirements'",
                 "sudo pip install -r /var/www/html/requirements.txt",
+
                 "cd /var/www/html/",
                 "sudo django-admin startproject aussichtsturm",
                 "cd /var/www/html/aussichtsturm/",
@@ -189,7 +190,12 @@ resource "aws_instance" "web-server-instance" {
                 "sudo chmod -R 777 /var/www/html/aussichtsturm/",
                 "sudo chmod 775 /var/www/html/aussichtsturm/db.sqlite3",
                 "sudo chown :www-data -R /var/www/html/aussichtsturm/",
-                
+
+                "cd /var/www/html/aussichtsturm/",
+                "sudo python3 manage.py collectstatic",
+                "sudo cp -r static/* youtube_comments_grabber/static/",
+
+
                 "SEC_K=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')",
                 "sudo echo SECRET_KEY=$SEC_K > /var/www/html/aussichtsturm/aussichtsturm/.env",
                 "sudo chmod -R 775 /var/www/html/aussichtsturm/",
